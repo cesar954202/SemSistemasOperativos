69d2aa566253205549cc6e4b0a601e3369bfcea1
Unknown <cesar95.4202@gmail.com>
2018-09-06 10:06:47 -0500

    objeto proceso

---
+++ Tarea1.py
@@ -0,0 +1,10 @@
+
+class proceso:
+    def __init__(self,o1,o1,s):
+        self.operador1 = o1
+        self.operador2 = o2
+        self.signo = s
+
+        
+    def mostrar(self):
+        print "Dato: "  , self.dato  , " Tipo: " , self.tipo
